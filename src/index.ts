ed22a7dd12091804ff0b4a1252edca2403b272a4
GweesinChan <gweesin2000@gmail.com>
2025-05-17 15:32:35 +0800

    feat!: rename export iconifyJSON as biliIconifyJSON

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import type { IconifyJSON } from '@iconify/types'
 import bili from '../json/bili.json'
 
-export const iconifyJSON = bili as IconifyJSON
+export const biliIconifyJSON = bili as IconifyJSON
