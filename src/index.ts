6dc2b753a16cb2fec5732b5e6cd3799c7e92fb68
gweesin <gweesin2000@gmail.com>
2025-06-24 11:44:23 +0800

    feat: support douyin(抖音) emote icons

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import type { IconifyJSON } from '@iconify/types'
 import bili from '../json/bili.json'
+import douyin from '../json/douyin.json'
 import zhihu from '../json/zhihu.json'
 
 export const biliIconifyJSON = bili as IconifyJSON
 export const zhihuIconifyJSON = zhihu as IconifyJSON
+export const douyinIconifyJSON = douyin as IconifyJSON
